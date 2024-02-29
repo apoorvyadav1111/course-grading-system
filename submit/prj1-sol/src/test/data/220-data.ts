af561db47ce76aa9673820f930a7292dba7c5235
ayadav16 <ayadav7@binghamton.edu>
2024-02-29 09:24:53 -0500

    first commit

---
+++ submit/prj1-sol/src/test/data/220-data.ts
@@ -0,0 +1,8 @@
+export const RAW = `
+firstName,lastName,emailId,prj1,prj2,prj3,hw1,hw2,hw3,qz1,qz2,qz3,lab,exam1,exam2,exam3
+`;
+
+export const FULL = `
+$stat,firstName,lastName,emailId,prj1,prj2,prj3,hw1,hw2,hw3,qz1,qz2,qz3,lab,exam1,exam2,exam3,prjAvg,hwAvg,qzAvg,examAvg,total
+`;
+
---
+++ submit/prj1-sol/src/test/data/544-data.ts
@@ -0,0 +1,7 @@
+export const RAW = `
+lastName,firstName,section,bNumber,emailId,prj1,prj2,prj3,prj4,hw1,hw2,hw3,hw4,qz1,qz2,qz3,qz4,midterm,final,paper,extra
+`;
+
+export const FULL = `
+$stat,lastName,firstName,section,bNumber,emailId,prj1,prj2,prj3,prj4,hw1,hw2,hw3,hw4,qz1,qz2,qz3,qz4,midterm,final,paper,extra,prjAvg,hwAvg,qzAvg,total
+`;
